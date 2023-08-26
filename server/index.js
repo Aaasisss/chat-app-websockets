cf625cc672e639a0b1ecddbcce4fa323ec044591
Ashis Chaudhary <ashischaudhary959@gmail.com>
2023-08-26 21:17:14 +1000

    initalise the app

---
+++ server/index.js
@@ -0,0 +1,22 @@
+const express = require("express");
+const app = express();
+const http = require("http");
+const server = http.createServer(app);
+const { Server } = require("socket.io");
+const cors = require("cors");
+app.use(cors());
+const io = new Server(server, {
+  cors: { origin: "http://localhost:3001", methods: ["GET", "POST"] },
+});
+
+app.get("/", (req, res) => {
+  res.sendFile(__dirname + "/index.html");
+});
+
+io.on("connection", (socket) => {
+  console.log("a user connected");
+});
+
+server.listen(3000, () => {
+  console.log("listening on *:3000");
+});
